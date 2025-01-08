25da3f3ba7768faa836ac2b59ef1979d0061aab6
derjulian2 <Julian.Benzel@gmail.com>
2025-01-08 16:31:37 +0100

    Update main.cpp

--- ThuliumLibrary/main.cpp
+++ ThuliumLibrary/main.cpp
@@ -1,6 +1,7 @@
 #include "Thulium.hpp"
 
 // make color transition stuff at edges of windows
+// use std::list instead of std::vector, because this doesnt invalidate references/pointers to the objects when adding/removing/moving objects
 
 int main(int argc, char** argv) {
     thl::Window window(500, 500, "iltam");
