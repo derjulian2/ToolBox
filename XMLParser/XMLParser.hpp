da3597116ab5a178c14118a40ac6475efde6c82d
derjulian2 <Julian.Benzel@gmail.com>
2025-01-13 17:39:34 +0100

    Update XMLParser.hpp

--- XMLParser/XMLParser.hpp
+++ XMLParser/XMLParser.hpp
@@ -2,8 +2,10 @@
 /*
 * basic XML-Parser
 * 
-* supports regular XML (tag and value pairs), self-closing tags, 
-* processing instructions (?xml...) and attributes
+* supports:
+* - 'standard' XML (tag and value pairs, optionally attributes)
+* - self-closing tags (<tag/>), 
+* - processing instructions (<?xml...?>)
 * 
 * use XMLParser::parseXMLString() to parse a string into an XMLMessage
 * use XMLMessage and XMLMessage::AddTag() and the returned XMLTag& as well as it's toString() to construct an XML-message
