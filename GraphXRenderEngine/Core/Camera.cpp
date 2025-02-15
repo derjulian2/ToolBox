3424fb0b974bfdfdce6405c879f6c6a404fffb02
derjulian2 <Julian.Benzel@gmail.com>
2025-02-15 17:17:22 +0100

    Add files via upload

--- GraphXRenderEngine/Core/Camera.cpp
+++ GraphXRenderEngine/Core/Camera.cpp
@@ -1,1 +1,76 @@
+//////////////////////////////////////////////////
+#include "Camera.hpp"
+//////////////////////////////////////////////////
+grx::Camera::Camera(glm::vec3 pos, float fov)
+	: pos(pos), up(glm::vec3(0.0f, 1.0f, 0.0f)), fov(fov)
+{
+	setDir(yaw, pitch);
+	calculateMatrix();
+}
+void grx::Camera::calculateMatrix()
+{
+	right = glm::normalize(glm::cross(up, dir));
+	view_matrix = glm::lookAt(
+		pos,
+		pos + dir,
+		up
+	);
+}
+void grx::Camera::setMatrix(const ShaderProgram& program) const
+{
+	program.setUniform("view_matrix", view_matrix);
+}
+//////////////////////////////////////////////////
+glm::vec3 grx::Camera::getPos() const
+{
+	return pos;
+}
+glm::vec3 grx::Camera::getDir() const
+{
+	return dir;
+}
+glm::vec2 grx::Camera::getYawAndPitch() const
+{
+	return glm::vec2(yaw, pitch);
+}
+glm::vec3 grx::Camera::getUp() const
+{
+	return up;
+}
+float grx::Camera::getFov() const
+{
+	return fov;
+}
+float grx::Camera::getSpeed() const
+{
+	return speed;
+}
+//////////////////////////////////////////////////
+void grx::Camera::setPos(glm::vec3 pos)
+{
+	this->pos = pos;
+	calculateMatrix();
+}
+void grx::Camera::setDir(float yaw, float pitch)
+{
+	this->yaw = yaw;
+	this->pitch = pitch;
 
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	dir = glm::vec3(
+		cos(glm::radians(yaw)) * cos(glm::radians(pitch)),
+	    sin(glm::radians(pitch)),
+	    sin(glm::radians(yaw)) * cos(glm::radians(pitch))
+		);
+	dir = glm::normalize(dir);
+	calculateMatrix();
+}
+void grx::Camera::setSpeed(float speed)
+{
+	this->speed = speed;
+}
+//////////////////////////////////////////////////
---
+++ GraphXRenderEngine/Core/VertexManagment.hpp
@@ -0,0 +1,126 @@
+//////////////////////////////////////////////////
+#ifndef GRX_VERTEX_MANAGEMENT_H
+#define GRX_VERTEX_MANAGEMENT_H
+//////////////////////////////////////////////////
+#include <cstdint>
+#include <stdexcept>
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+//////////////////////////////////////////////////
+namespace grx
+{
+	//////////////////////////////////////////////////
+
+	struct Color
+	{
+		explicit Color(float r, float g, float b, float a);
+		explicit Color(glm::vec4 rgba = glm::vec4(0, 0, 0, 0));
+		float r;
+		float g;
+		float b;
+		float a;
+	};
+
+	//////////////////////////////////////////////////
+
+	struct Vertex
+	{
+		Vertex(glm::vec3 pos = glm::vec3(0, 0, 0),
+			Color color = Color(glm::vec4(0, 0, 0, 0)),
+			glm::vec2 texcoords = glm::vec2(0, 0));
+
+		glm::vec3 pos;
+		Color color;
+		glm::vec2 texcoords;
+	};
+
+	//////////////////////////////////////////////////
+
+	typedef unsigned int VertexArrayHandle;
+
+	class VertexArray
+	{
+	public:
+		VertexArray();
+		VertexArray(const VertexArray&) = delete;
+		VertexArray(VertexArray&&) noexcept;
+		VertexArray& operator=(const VertexArray&) = delete;
+		const VertexArray& operator=(VertexArray&&) noexcept;
+		~VertexArray();
+
+		VertexArrayHandle getHandle() const;
+		void release();
+	private:
+		VertexArrayHandle vertex_array_handle;
+	};
+
+	typedef unsigned int IndexBufferHandle;
+
+	class IndexBuffer
+	{
+	public:
+		enum BufferMode
+		{
+			STREAM,
+			STATIC,
+			DYNAMIC
+		};
+
+		IndexBuffer();
+		IndexBuffer(const VertexArray& vao,
+			uint32_t* data,
+			size_t size,
+			BufferMode mode = STATIC);
+		IndexBuffer(const IndexBuffer&) = delete;
+		IndexBuffer(IndexBuffer&&) noexcept;
+		IndexBuffer& operator=(const IndexBuffer&) = delete;
+		const IndexBuffer& operator=(IndexBuffer&&) noexcept;
+		~IndexBuffer();
+
+		void BufferData(const VertexArray& vao,
+			uint32_t* data,
+			size_t size,
+			BufferMode mode = STATIC) const;
+
+		IndexBufferHandle getHandle() const;
+		void release();
+	private:
+		IndexBufferHandle index_buffer_handle;
+	};
+
+	typedef unsigned int VertexBufferHandle;
+
+	class VertexBuffer
+	{
+	public:
+		enum BufferMode
+		{
+			STREAM,
+			STATIC,
+			DYNAMIC
+		};
+
+		VertexBuffer();
+		VertexBuffer(VertexArray& vao, const Vertex* data, size_t size, BufferMode mode = STATIC);
+		VertexBuffer(const VertexBuffer&) noexcept;
+		VertexBuffer(VertexBuffer&&) noexcept;
+		VertexBuffer& operator=(const VertexBuffer&) noexcept;
+		const VertexBuffer& operator=(VertexBuffer&&) noexcept;
+		~VertexBuffer();
+
+		void BufferData(const VertexArray& vao,
+			const Vertex* data,
+			size_t size,
+			BufferMode mode = STATIC) const;
+
+		VertexBufferHandle getHandle() const;
+		void release();
+	protected:
+		VertexBufferHandle vertex_buffer_handle;
+	};
+
+	//////////////////////////////////////////////////
+}
+//////////////////////////////////////////////////
+#endif
+//////////////////////////////////////////////////
