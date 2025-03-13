e84bc7281c01f76cae78dd6ee58c8f179afacf89
derjulian2 <Julian.Benzel@gmail.com>
2025-03-13 22:19:29 +0100

    Add files via upload

---
+++ utilitylib/include/utilitylib.hpp
@@ -0,0 +1,13 @@
+////////////////////////////////////////
+/// general utility header-only-library
+/// for convenience methods/types in C++
+/// 2025 Julian Benzel
+////////////////////////////////////////
+/// include all header
+////////////////////////////////////////
+#include "utilitylib/io.hpp"
+#include "utilitylib/random.hpp"
+#include "utilitylib/regex.hpp"
+#include "utilitylib/stringmanip.hpp"
+#include "utilitylib/time.hpp"
+////////////////////////////////////////
