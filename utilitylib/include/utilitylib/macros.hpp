8558be76a10125fc67d7934575bfd4b0e89ff491
derjulian2 <Julian.Benzel@gmail.com>
2025-03-17 20:04:05 +0100

    Update macros.hpp

--- utilitylib/include/utilitylib/macros.hpp
+++ utilitylib/include/utilitylib/macros.hpp
@@ -16,8 +16,20 @@ inline type get_##name() const \
 	return name; \
 }
 
+#define UTIL_GET_REF(type, name) \
+inline type& getref_##name() \
+{ \
+	return name; \
+}
+
 #define UTIL_SET(type, name) \
-inline void set_##name(type t) \
+inline void set_##name(const type& t) \
+{ \
+	this->name = t; \
+}
+
+#define UTIL_SET_VAL(type, name) \
+inline void setval_##name(type t) \
 { \
 	this->name = t; \
 }
